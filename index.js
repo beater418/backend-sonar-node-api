8e63840f1b376fbbcc90fc35b4f95a03a31bffba
Luis Fernando <lcruzf@intercorp.com.pe>
2026-09-09 20:37:09 -0500

    add: message api

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.get('/api/hello', (req, res) => {
     console.log('---INVOCANDO SERVICIO HELLO');
-    res.json({message: 'Hola mundo!'});
+    res.json({message: 'Hola mundo workflow!'});
 })
 
 app.get('/api/message', (req, res) => {
